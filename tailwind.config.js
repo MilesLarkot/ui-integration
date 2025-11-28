5e108aff73132b02e6993d641009c0b4f6cc2df4
Miles Larkot <mileslarkot@proton.me>
2025-11-28 17:52:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1A3D64",
+        secondary: "#0C2B4E",
+        accent: "#1D546C",
+        neutrallight: "#F4F4F4",
+        neutraldark: "#454545",
+      },
+    },
+  },
+};
